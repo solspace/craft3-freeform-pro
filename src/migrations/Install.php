fcc11414e32d0f6e79ebf06674713f8ad81c968d
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2018-01-25 16:21:15 +0200

    v2.0.0-beta.2

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -2,90 +2,37 @@
 
 namespace Solspace\FreeformPro\migrations;
 
-use craft\db\Migration;
+use Solspace\Commons\Migrations\ForeignKey;
+use Solspace\Commons\Migrations\StreamlinedInstallMigration;
+use Solspace\Commons\Migrations\Table;
 
 /**
  * Install migration.
  */
-class Install extends Migration
+class Install extends StreamlinedInstallMigration
 {
     /**
-     * @inheritdoc
+     * @return Table[]
      */
-    public function safeUp()
-    {
-        foreach ($this->getTableData() as $data) {
-            $options               = $data['options'] ?? null;
-            $fields                = $data['fields'];
-            $fields['dateCreated'] = $this->dateTime()->notNull()->defaultExpression('NOW()');
-            $fields['dateUpdated'] = $this
-                    ->dateTime()
-                    ->notNull()
-                    ->defaultExpression('NOW()') . ' ON UPDATE CURRENT_TIMESTAMP';
-            $fields['uid']         = $this->char(36)->defaultValue(0);
-
-            $this->createTable($data['table'], $fields, $options);
-        }
-
-        $this->addForeignKey(
-            'export_profiles_formId',
-            'freeform_export_profiles',
-            'formId',
-            'freeform_forms',
-            'id',
-            'CASCADE'
-        );
-
-        $this->addForeignKey(
-            'export_settings_userId',
-            'freeform_export_settings',
-            'userId',
-            'users',
-            'id',
-            'CASCADE'
-        );
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function safeDown()
-    {
-        $this->dropForeignKey('export_profiles_formId', 'freeform_export_profiles');
-        $this->dropForeignKey('export_settings_userId', 'freeform_export_settings');
-
-        foreach ($this->getTableData() as $data) {
-            $this->dropTableIfExists($data['table']);
-        }
-    }
-
-    /**
-     * @return array
-     */
-    private function getTableData(): array
+    protected function defineTableData(): array
     {
         return [
-            [
-                'table'  => 'freeform_export_profiles',
-                'fields' => [
-                    'id'        => $this->primaryKey(),
-                    'formId'    => $this->integer()->notNull(),
-                    'name'      => $this->string(255)->notNull()->unique(),
-                    'limit'     => $this->integer(),
-                    'dateRange' => $this->string(255),
-                    'fields'    => $this->text()->notNull(),
-                    'filters'   => $this->text(),
-                    'statuses'  => $this->text()->notNull(),
-                ],
-            ],
-            [
-                'table'  => 'freeform_export_settings',
-                'fields' => [
-                    'id'      => $this->primaryKey(),
-                    'userId'  => $this->integer()->notNull(),
-                    'setting' => $this->text(),
-                ],
-            ],
+            (new Table('freeform_export_profiles'))
+                ->addField('id', $this->primaryKey())
+                ->addField('formId', $this->integer()->notNull())
+                ->addField('name', $this->string(255)->notNull()->unique())
+                ->addField('limit', $this->integer())
+                ->addField('dateRange', $this->string(255))
+                ->addField('fields', $this->text()->notNull())
+                ->addField('filters', $this->text())
+                ->addField('statuses', $this->text()->notNull())
+                ->addForeignKey('formId', 'freeform_forms', 'id', ForeignKey::CASCADE),
+
+            (new Table('freeform_export_settings'))
+                ->addField('id', $this->primaryKey())
+                ->addField('userId', $this->integer()->notNull())
+                ->addField('setting', $this->text())
+                ->addForeignKey('userId', 'users', 'id', ForeignKey::CASCADE),
         ];
     }
 }
