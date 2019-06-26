bcf050212c61e299e1326093adc85b5569647cdb
Kelsey Martens <kelsey@solspace.com>
2019-06-26 11:58:06 -0500

    v2.5.26

--- src/FreeformPro.php
+++ src/FreeformPro.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: gustavs
- * Date: 31/08/2017
- * Time: 12:01
+ * Freeform for Craft
+ *
+ * @package       Solspace:Freeform
+ * @author        Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2019, Solspace, Inc.
+ * @link          https://docs.solspace.com/craft/freeform
+ * @license       https://docs.solspace.com/license-agreement
  */
 
 namespace Solspace\FreeformPro;
